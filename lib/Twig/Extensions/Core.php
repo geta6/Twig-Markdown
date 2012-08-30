6396ab9fc4eb6ce4ffb37937b141f4420ad1accb
Gourry <gourrymk2@gmail.com>
2012-08-30 18:55:07 +0800

    include "markdown.php" before include the Twig-Markdown extension
    it is better than include "markdown.php" everywhere in this library

--- lib/Twig/Extensions/Core.php
+++ lib/Twig/Extensions/Core.php
@@ -8,7 +8,6 @@ class Twig_Markdown_Extension extends Twig_Extension
 {
     public function getTokenParsers()
     {
-        include_once dirname(__FILE__).'/../TokenParser/Markdown.php';
         return array(
             new Twig_TokenParser_Markdown(),
         );
@@ -33,7 +32,6 @@ class Twig_Markdown_Extension extends Twig_Extension
 
 function twig_markdown($data)
 {
-    include_once dirname(__FILE__).'/../Markdown/markdown.php';
     return Markdown($data);
 }
 
